d5e836b624d5c5257365ffd8215beaf40fe75ed9
AndewSteve <2535023693@qq.com>
2023-08-31 01:06:37 +0800

    学习关联网页模板

--- E624V/ScreenApp/views.py
+++ E624V/ScreenApp/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
-def index(request):
-    return HttpResponse("Fuck")
+def home(request):
+    return render(request,'home.html',{})
